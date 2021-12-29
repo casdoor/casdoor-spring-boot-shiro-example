deeb05af9ed0824186e4d4395089dc2d68e8e4db
Yixiang Zhao <seriouszyx@foxmail.com>
2021-12-29 20:54:41 +0800

    chore: init spring boot project
    Signed-off-by: Yixiang Zhao <seriouszyx@foxmail.com>

---
+++ src/main/java/com/casbin/shiro/example/CasdoorSpringBootShiroExampleApplication.java
@@ -0,0 +1,13 @@
+package com.casbin.shiro.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasdoorSpringBootShiroExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CasdoorSpringBootShiroExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/casbin/shiro/example/CasdoorSpringBootShiroExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casbin.shiro.example;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasdoorSpringBootShiroExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
